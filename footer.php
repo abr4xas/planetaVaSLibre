5f492b7524a96103921f2d067f8f2c5025614b1d
abr4xas <me@abr4xas.org>
2015-01-15 19:17:54 -0430

    Agregado popup para las politicas de uso

--- footer.php
+++ footer.php
@@ -14,11 +14,11 @@ echo '<div class="row">
          </div>
          <address>
             <strong>Planeta de Grupo de Usuarios de VaSlibre</strong><br />
-            Valencia - Carabobo - Venezuela<br />
-            <a href="index.php?d=politicas" target="_self" title="Leer politicas de uso">Pol&iacute;ticas de uso</a>
-         </address>
+            Valencia - Carabobo - Venezuela<br />';
+            echo "<a href=\"#\" onclick=\"javascript:void window.open('include/politicas.php','1421363962543','width=700,height=500,toolbar=0,menubar=0,location=0,status=0,scrollbars=1,resizable=1,left=0,top=0');return false;\">Politicas de uso</a>";           
+echo '</address>
          &copy; 2014 - '.date("Y",time()).'  Dise&ntilde;o & Programaci&oacute;n: [ <a href="http://viserproject.com" target="_blank" title="Visitar ViserProject">ViSerProject</a> ]<br />
          C&oacute;digo fuente: <a href="https://github.com/Vaslibre/planeta" target="_blank" title="Código Fuente del Planeta">github.com</a>                    
        </div>
      </div>';
-?>                     
+?>
